b2f2d31b58c8d7c422920be818927dfb269d3c64
Kartick17 <karticksadhu0@gmail.com>
2023-08-04 03:25:01 +0530

    Implement nested route on tour route

--- controllers/authController.js
+++ controllers/authController.js
@@ -95,7 +95,7 @@ exports.protect = catchAsync(async (req, res, next) => {
         );
     }
 
-    // GRANT ACCESS TO PROTECTED ROUTE 
+    // GRANT ACCESS USER TO PROTECTED ROUTE 
     req.user = currentUser;
     next();
 })
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const tourController = require('./../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewRoutes = require('./reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', checkId);
+
+// Nested routes(If review found in URL so redirect to review route)
+router.use('/:tourId/reviews', reviewRoutes);
 
 router.route('/best-5-economical').get(tourController.aliasTopTours, tourController.getAllTours)
 router.route('/tour-stats').get(tourController.getTourStats)
