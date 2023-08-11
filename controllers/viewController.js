6950cfe3c3748ce0ef8f6ea04130aa658957455e
Kartick17 <karticksadhu0@gmail.com>
2023-08-12 00:31:03 +0530

    Building tour page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -12,8 +12,15 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     })
 })
 
-exports.getTour = (req, res) => {
-    res.status(200).render('tour', {
-        title: 'All Tours'
+exports.getTour = catchAsync(async (req, res, next) => {
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user'
     })
-}
+
+    console.log(tour);
+    res.status(200).render('tour', {
+        title: tour.name,
+        tour
+    })
+})
