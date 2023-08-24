4501bebb68dfe11474e941b09048ccd2618c9f68
Kartick17 <karticksadhu0@gmail.com>
2023-08-25 01:55:33 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-// const https = require('https');
+const https = require('https');
 const fs = require('fs')
 
 process.on('uncaughtException', err => {
@@ -21,22 +21,22 @@ mongoose.connect(DB, {
     useUnifiedTopology: true
 }).then(() => console.log('Database connection successful'));
 
-// const options = {
-//     key: fs.readFileSync('./config/cert.key'),
-//     cert: fs.readFileSync('./config/cert.crt')
-// };
+const options = {
+    key: fs.readFileSync('./config/cert.key'),
+    cert: fs.readFileSync('./config/cert.crt')
+};
 
 const port = process.env.PORT || 5000;
 
 // create http server 
-const server = app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
-});
-
-// create https server 
-// const server = https.createServer(options, app).listen(port, () => {
+// const server = app.listen(port, () => {
 //     console.log(`App running on port ${port}...`);
 // });
+
+// create https server 
+const server = https.createServer(options, app).listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
 
 process.on('unhandledRejection', err => {
     console.log('UNHANDLED REJECTION! 💥 Sutting Down...');
