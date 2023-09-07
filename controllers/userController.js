04741d39e4467b28635aab14c0b6f5c21115f506
kartick17 <karticksadhu0@gmail.com>
2023-09-07 20:25:24 +0530

    Upload user photo into AWS S3

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,76 @@
-const multer = require('multer');
 const sharp = require('sharp');
+const AWS = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
 const User = require('./../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-const multerStorate = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/img/users')
-    },
-    filename: (req, file, cb) => {
-        const ext = file.mimetype.split('/')[1];
-        cb(null, `users-${req.user.id}-${Date.now()}.${ext}`);
-    }
-})
+require('aws-sdk/lib/maintenance_mode_message').suppress = true;
 
-// const multerStorate = multer.memoryStorage();
+// AWS s3 configuration
+const s3Config = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    Bucket: process.env.AWS_BUCKET_NAME,
+    signatureVersion: 'v4',
+    region: process.env.AWS_REGION
+});
 
+// Check file is image
 const multerFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image'))
+    if (file.mimetype.startsWith('image')) {
         cb(null, true);
+    }
     else
         cb(new AppError('Not an image! Please upload only image', 400));
 }
 
-const upload = multer({
-    storage: multerStorate,
-    fileFilter: multerFilter
+// Create storage using s3-multer
+const multerS3Config = multerS3({
+    s3: s3Config,
+    bucket: 'natours-image',
+    metadata: function (req, file, cb) {
+        cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+        multerFilter(req, file, cb);
+        const ext = file.mimetype.split('/')[1];
+        file.originalname = `users-${req.user.id}-${Date.now()}.${ext}`
+        cb(null, file.originalname)
+    }
 });
+
+
+// const multerStorate = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'public/img/users')
+//     },
+//     filename: (req, file, cb) => {
+//         const ext = file.mimetype.split('/')[1];
+//         cb(null, `users-${req.user.id}-${Date.now()}.${ext}`);
+//     }
+// })
+
+// const multerStorate = multer.memoryStorage();
+
+// For store file locally
+// const upload = multer({
+//     storage: multerStorate,
+//     fileFilter: multerFilter
+// });
+
+// Upload file in s3 bucket
+const upload = multer({
+    storage: multerS3Config,
+    // fileFilter: fileFilter,
+    limits: {
+        fileSize: 1024 * 1024 * 5 // we are allowing only 5 MB files
+    }
+})
+
 
 exports.uploadUserPhoto = upload.single('photo');
 
@@ -62,7 +105,8 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
     // 2) Filtered out unwanted fields names that are not allowed to be updated
     const filteredBody = filterObj(req.body, 'name', 'email');
-    if (req.file) filteredBody.photo = req.file.filename;
+    console.log(req.file.originalname);
+    if (req.file) filteredBody.photo = req.file.originalname;
 
     // 3) Update user document
     const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
